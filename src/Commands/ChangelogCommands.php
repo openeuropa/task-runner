b35fc80b7241f40882b40c8dc11e0bad293bbbd7
Antonio De Marco <antonio@nuvole.org>
2017-12-29 13:56:50 +0100

    Issue #4: Add simulation tests.

--- src/Commands/ChangelogCommands.php
+++ src/Commands/ChangelogCommands.php
@@ -12,7 +12,17 @@ use Symfony\Component\Console\Input\InputOption;
 class ChangelogCommands extends BaseCommands
 {
     /**
+     * Generate a changelog based on GitHub issues and pull requests.
+     *
+     * A running Docker installation is required to run this command.
+     * For more information check https://github.com/skywinder/github-changelog-generator
+     *
      * @command changelog:generate
+     *
+     * @option token GitHub authentication token, to generate one visit https://github.com/settings/tokens/new
+     * @option tag   Specify the future release version you wish to generate the changelog for.
+     *
+     * @aliases changelog:g,cg
      *
      * @param array $options
      *
@@ -24,15 +34,16 @@ class ChangelogCommands extends BaseCommands
     ])
     {
         $projectName = $this->getFullProjectName();
-        $image = "muccg/github-changelog-generator {$projectName} -t {$options['token']}";
+        $exec = "{$projectName} -t {$options['token']}";
         if (!empty($options['tag'])) {
-            $image .= " --future-release={$options['tag']}";
+            $exec .= " --future-release={$options['tag']}";
         }
 
-        $task = $this->taskDockerRun($image)
+        $task = $this->taskDockerRun('muccg/github-changelog-generator')
           ->option('rm')
           ->rawArg('-v $(pwd):$(pwd)')
-          ->rawArg('-w $(pwd)');
+          ->rawArg('-w $(pwd)')
+          ->exec($exec);
 
         return $task;
     }
