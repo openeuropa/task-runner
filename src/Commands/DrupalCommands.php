34b8601efcafed90418906f50461b55f90b060a6
Antonio De Marco <antonio@nuvole.org>
2018-01-15 19:39:22 +0100

    Issue #29: Add php server command.

--- src/Commands/DrupalCommands.php
+++ src/Commands/DrupalCommands.php
@@ -244,4 +244,39 @@ class DrupalCommands extends AbstractCommands implements FilesystemAwareInterfac
             $this->taskAppendConfiguration($options['root'].'/sites/default/default.settings.php', $this->getConfig())->setConfigKey('drupal.settings'),
         ]);
     }
+
+    /**
+     * Run PHP server.
+     *
+     * Run a PHP server using default configuration values provided in local
+     * runner.yml.dist/runner.yml files.
+     *
+     * @command drupal:run-server
+     *
+     * @option root         Drupal site root.
+     * @option base-url     Drupal site base URL
+     * @option background   Run in background.
+     *
+     * @aliases drupal:rs
+     *
+     * @param array $options
+     *
+     * @return \Robo\Contract\TaskInterface
+     */
+    public function runServer(array $options = [
+      'root'       => InputOption::VALUE_REQUIRED,
+      'base-url'   => InputOption::VALUE_REQUIRED,
+      'background' => InputOption::VALUE_NONE,
+    ])
+    {
+        $host = parse_url($options['base-url'], PHP_URL_HOST);
+        $port = parse_url($options['base-url'], PHP_URL_PORT);
+        $port = ($port === null) ? 80 : $port;
+
+        return $this->taskServer($port)
+          ->rawArg('-dalways_populate_raw_post_data=-1')
+          ->host($host)
+          ->dir($options['root'])
+          ->background($options['background']);
+    }
 }
