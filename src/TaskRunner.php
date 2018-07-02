5788ddc3618082f1506820903088dccc2fea2368
Pol Dellaiera <pol.dellaiera@protonmail.com>
2018-07-02 14:33:43 +0200

    OPENEUROPA-544: Remove the class discovery mechanism which is not using a plugin based approach in Robo.

--- src/TaskRunner.php
+++ src/TaskRunner.php
@@ -90,63 +90,11 @@ class TaskRunner
      */
     public function run()
     {
-        // Register command classes.
-        $this->runner->registerCommandClasses($this->application, $this->getCommandDiscovery()->discover(__DIR__, 'OpenEuropa\\TaskRunner'));
-
         // Register commands defined in runner.yml file.
         $this->registerDynamicCommands($this->application);
 
         // Run command.
         return $this->runner->run($this->input, $this->output, $this->application);
-    }
-
-    /**
-     * @param string $class
-     *
-     * @return \OpenEuropa\TaskRunner\Commands\AbstractCommands
-     *
-     * @throws \Psr\Container\ContainerExceptionInterface
-     * @throws \Psr\Container\NotFoundExceptionInterface
-     */
-    public function getCommands($class)
-    {
-        // Register command classes.
-        $this->runner->registerCommandClasses($this->application, $this->getCommandDiscovery()->discover(__DIR__, 'OpenEuropa\\TaskRunner'));
-
-        return $this->getContainer()->get("{$class}Commands");
-    }
-
-    /**
-     * @param \Composer\Autoload\ClassLoader $classLoader
-     */
-    public function registerExternalCommands(ClassLoader $classLoader)
-    {
-        $commands = [];
-        $discovery = $this->getCommandDiscovery();
-
-        foreach ($classLoader->getPrefixesPsr4() as $baseNamespace => $directoryList) {
-            $directoryList = array_filter($directoryList, function ($path) {
-                return is_dir($path.'/TaskRunner/Commands');
-            });
-
-            if (!empty($directoryList)) {
-                $discoveredCommands = $discovery->discover($directoryList, $baseNamespace);
-                $commands = array_merge($commands, $discoveredCommands);
-            }
-        }
-
-        $this->runner->registerCommandClasses($this->application, $commands);
-    }
-
-    /**
-     * @return \Consolidation\AnnotatedCommand\CommandFileDiscovery
-     */
-    private function getCommandDiscovery()
-    {
-        $discovery = new CommandFileDiscovery();
-        $discovery->setSearchPattern('*Commands.php')->setSearchLocations(['TaskRunner', 'Commands']);
-
-        return $discovery;
     }
 
     /**
