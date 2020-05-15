8be1b2efcaa2bad7aacee137afee9fff9a525150
Claudiu Cristea <clau.cristea@gmail.com>
2020-05-15 11:50:39 +0300

    ISAICP-5825: Fix typo.

--- src/Tasks/CollectionFactory/CollectionFactory.php
+++ src/Tasks/CollectionFactory/CollectionFactory.php
@@ -95,7 +95,7 @@ class CollectionFactory extends BaseTask implements BuilderAwareInterface, Simul
     protected function taskFactory($task)
     {
         if (is_string($task)) {
-            @trigger_error('Defining a task as a plain text is depracted. Use the "exec" task and pass arguments and options.', E_USER_DEPRECATED);
+            @trigger_error('Defining a task as a plain text is deprecated. Use the "exec" task and pass arguments and options.', E_USER_DEPRECATED);
             return $this->taskExec($task)->interactive($this->isTtySupported());
         }
 
