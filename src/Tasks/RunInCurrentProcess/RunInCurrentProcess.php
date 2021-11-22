4e37fb292b39c296cd3e84e0ce8eb17429973f01
Merlin <merlin@geeks4change.net>
2021-11-22 16:38:06 +0100

    Fix indentation

--- src/Tasks/RunInCurrentProcess/RunInCurrentProcess.php
+++ src/Tasks/RunInCurrentProcess/RunInCurrentProcess.php
@@ -4,6 +4,7 @@ namespace OpenEuropa\TaskRunner\Tasks\RunInCurrentProcess;
 
 use Consolidation\Config\ConfigInterface;
 use Robo\Common\CommandArguments;
+use Robo\Result;
 use Robo\Robo;
 use Robo\Runner as RoboRunner;
 use Robo\Task\BaseTask;
@@ -12,40 +13,41 @@ use Symfony\Component\Console\Input\StringInput;
 /**
  * Run a configurable command in current process.
  */
-class RunInCurrentProcess extends BaseTask {
+class RunInCurrentProcess extends BaseTask
+{
 
-  use CommandArguments;
+    use CommandArguments;
 
-  /**
-   * @var string
-   */
-  protected $command;
+    /**
+     * @var string
+     */
+    protected $command;
 
-  /**
-   * @param string $command
-   */
-  public function __construct(string $command)
-  {
-    $this->command = $command;
-  }
+    /**
+     * @param string $command
+     */
+    public function __construct(string $command)
+    {
+        $this->command = $command;
+    }
 
-  public function run() {
-    // Backup config, as command may change it.
-    $container = Robo::getContainer();
-    $config = $container->get('config');
-    assert($config instanceof ConfigInterface);
-    $configExport = $config->export();
+    public function run() {
+        // Backup config, as command may change it.
+        $container = Robo::getContainer();
+        $config = $container->get('config');
+        assert($config instanceof ConfigInterface);
+        $configExport = $config->export();
 
-    // Assemble and run command.
-    $line = trim($this->command . $this->arguments);
-    $input = new StringInput($line);
-    $runner = new RoboRunner();
-    $runner->setContainer($container);
-    $statusCode = $runner->run($input, Robo::output());
+        // Assemble and run command.
+        $line = trim($this->command . $this->arguments);
+        $input = new StringInput($line);
+        $runner = new RoboRunner();
+        $runner->setContainer($container);
+        $statusCode = $runner->run($input, Robo::output());
 
-    // Restore config.
-    $config->replace($configExport);
-    return $statusCode;
-  }
+        // Restore config.
+        $config->replace($configExport);
+        return $statusCode;
+    }
 
 }
--- src/Tasks/RunInCurrentProcess/loadTasks.php
+++ src/Tasks/RunInCurrentProcess/loadTasks.php
@@ -5,15 +5,16 @@ namespace OpenEuropa\TaskRunner\Tasks\RunInCurrentProcess;
 /**
  * Load run-in-current-process tasks.
  */
-trait loadTasks {
+trait loadTasks
+{
 
-  /**
-   * @param string|\Robo\Contract\CommandInterface $command
-   *
-   * @return \Robo\Task\Base\Exec|\Robo\Collection\CollectionBuilder
-   */
-  public function taskRunInCurrentProcess($command) {
-    return $this->task(RunInCurrentProcess::class, $command);
-  }
+    /**
+     * @param string|\Robo\Contract\CommandInterface $command
+     *
+     * @return \Robo\Task\Base\Exec|\Robo\Collection\CollectionBuilder
+     */
+    public function taskRunInCurrentProcess($command) {
+        return $this->task(RunInCurrentProcess::class, $command);
+    }
 
 }
